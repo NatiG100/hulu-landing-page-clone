9fbc8f3a501604ae412b3daddfeb639925023e2c
NatiG100 <kalebok39@gmail.com>
2022-04-04 03:33:49 -0700

    assets

---
+++ src/components/Footer/style.js
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled"
+
+export const StyledFooter = styled.div`
+`;
+export const StyledFooterNavigation = styled.div`
+`;
+export const StyledNavigationGroup = styled.div`
+`;
+export const StyledNavigationSubGroup = styled.div`
+`;
+export const StyledDivider = styled.div`
+`;
+export const StyledSocialMediaList = styled.div`
+`;
+export const StyledLinks = styled.div`
+`;
